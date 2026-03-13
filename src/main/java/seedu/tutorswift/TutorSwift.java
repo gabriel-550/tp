947cebdbf05091b94c61c431b9f9e7c659e960dd
Chanyf123 <chanyifeng123@gmail.com>
2026-03-13 17:24:39 +0800

    Amend logic flow for main class

--- src/main/java/seedu/tutorswift/TutorSwift.java
+++ src/main/java/seedu/tutorswift/TutorSwift.java
@@ -1,24 +1,39 @@
 package seedu.tutorswift;
 
+import seedu.tutorswift.command.Command;
+
+/**
+ * Main entry-point for the TutorSwift application.
+ */
 public class TutorSwift {
-    /**
-     * Main entry-point for the TutorSwift application.
-     */
-    public static void main(String[] args) {
-        Ui ui = new Ui();
+    private Ui ui;
+    private StudentList students;
 
+    public TutorSwift() {
+        ui = new Ui();
+        students = new StudentList();
+    }
+
+    public void run() {
         ui.showWelcome();
-
         boolean isExit = false;
         while (!isExit) {
-            String input = ui.readCommand();
+            try {
+                String userInput = ui.readCommand();
 
-            if (input.trim().equalsIgnoreCase("bye")) {
-                ui.showExit();
-                isExit = true;
+                // Parse user input into command object to determine command name
+                Command c = Parser.parseUserInput(userInput);
+                c.execute(students, ui);
+                isExit = c.isExit();
+                ui.close();
+            } catch (TutorSwiftException e) {
+                ui.showError(e.getMessage());
             }
         }
 
-        ui.close();
+    }
+
+    public static void main(String[] args) {
+        new TutorSwift().run();
     }
 }
