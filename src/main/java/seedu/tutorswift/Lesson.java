0ab754e7adea23bc1b6981df603c3dd7f6a94b75
Chanyf123 <chanyifeng123@gmail.com>
2026-03-27 11:51:42 +0800

    Implement lesson class to represent tutoring time slots

---
+++ src/main/java/seedu/tutorswift/Lesson.java
@@ -0,0 +1,35 @@
+package seedu.tutorswift;
+
+import java.time.DayOfWeek;
+import java.time.LocalTime;
+
+public class Lesson {
+    private final DayOfWeek day;
+    private final LocalTime startTime;
+    private final LocalTime endTime;
+
+    public Lesson(DayOfWeek day, LocalTime startTime, LocalTime endTime) throws TutorSwiftException {
+        if (startTime.isAfter(endTime) || startTime.equals(endTime)) {
+            throw new TutorSwiftException("End time must be after start time.");
+        }
+        this.day = day;
+        this.startTime = startTime;
+        this.endTime = endTime;
+    }
+
+    /**
+     * Checks if this lesson overlaps with another lesson.
+     */
+    public boolean isOverlapping(Lesson otherLesson) {
+        if (this.day != otherLesson.day) {
+            return false;
+        }
+        return this.startTime.isBefore(otherLesson.endTime) && otherLesson.startTime.isBefore(this.endTime);
+    }
+
+    @Override
+    public String toString() {
+        return day + " " + startTime + " to " + endTime;
+    }
+}
+
